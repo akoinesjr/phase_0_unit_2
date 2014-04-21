7e15508ab2645bc19de4280ae3c06220aec8ff80
Andrew Koines <akoines@zoho.com>
2014-04-21 14:53:04 -0400

    Added exercise 1 solutions

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,22 +4,94 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
+
+#Define function "total" that takes array as argument
+#	answer=0;
+#	FOR element in array
+#		ADD current element to answer
+#	ENDFOR
+#	puts answer
+#END
+
+#Define function "sentence_maker" that takes array of string as argument
+#  answer=""
+#  FOR element in array
+#    Add element to answer
+#  END FOR
+#  return answer
+#END
+
+
 # What is the input?
+
+#The input is the array argument
+
 # What is the output? (i.e. What should the code return?)
+
+#The output is the variable answer, which for the first method is the sum of the values 
+#of the elements in the array and for the second is the strings in the array concatenated 
+#together.
+
+
 # What are the steps needed to solve the problem?
+
+#Define the function. Write the loop. Return the answer.
 
 
 # 2. Initial Solution
 
+#def total(array)
+#	answer=0
+#	array.each do |x|
+#		answer+=x
+#	end
+#	return answer
+#end
 
+#def sentence_maker(array)
+#	answer=""
+#	array.each do |x|
+#		answer+=x+" "
+#	end
+#	answer+=""
+#	#puts answer
+#	return answer
+#end
 
 
 # 3. Refactored Solution
 
+#Realized that this could be done more efficiently with inject
+
+def total(array)
+	answer=array.inject(0){
+		|sum,i| sum+i
+	}
+	return answer
+end
+
+#Failed initially because function did not capitalize the first word and could not
+#handle numbers.
+
+def sentence_maker(array)
+	answer=""
+	array[0]=array[0].capitalize
+	array.each do |x|
+		x=x.to_s
+		answer+=x+" "
+	end	
+	answer=answer.chop+"."
+	return answer
+end
+
+sentence_maker(['a','c','dfg dsds'])
 
 
 # 4. Reflection 
+
+#Sometimes the method that comes to mind first or that you are most familiar 
+#with is not the best solution.
--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -4,22 +4,73 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
+#DEF function "mode" that takes an array as an argument
+# SET variable "count" to an empty hash
+#
+# FOR each element in array
+# 	Increment the value of the element key in the hash by one
+# ENDFOR
+#
+# SET variable mode_array to an empty array
+#
+# FOR each element in the hash
+# 	IF the value is the highest value in the high
+# 		push the corresponding key to mode_array
+# ENDFOR
+#
+# SORT mode_array
+#
+#END
+
+
 # What is the input?
+
+#An array of numbers
+
 # What is the output? (i.e. What should the code return?)
+
+#The mode.
+
 # What are the steps needed to solve the problem?
 
+#Determine how to count the instances of each number in the array
+#Create the loop to do this
+#Check to see which value was found the most times and return which one it is
 
 # 2. Initial Solution
 
+def mode(array)
 
+count=Hash.new(0)
+
+array.each do |x|
+  count[x]+=1
+end
+
+mode_array=[]
+
+count.each do |k,v|
+    if v == count.values.max
+        mode_array<<k
+    end
+end
+
+mode_array.sort
+
+end
 
 
 # 3. Refactored Solution
 
+#This solution worked and I do not see a way to improve it.
 
 
 # 4. Reflection 
+
+#Solving this problem was somewhat challenging as I had to determine how to
+#use a hash to count the instances of each number in the array, and then later
+#access these values based on certain criteria.
