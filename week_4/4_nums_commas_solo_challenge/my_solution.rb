472a36e0017b2ca9564be284786744c91d3ed21f
Andrew Koines <akoines@zoho.com>
2014-04-22 11:24:07 -0400

    ex 4 psuedocode

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -7,10 +7,29 @@
 
 # 1. Pseudocode
 
+#DEFINE a function that takes an argument "num" that is an integer
+#	IF the length of num is 3 or less
+#		RETURN num
+#	ELSE
+#			ADD a "," before the 3rd, 6th, 9th, etc. characters from the end of num
+#     RETURN num
+#	END
+
+#END
+
 # What is the input?
+
+#The input is an an integer
+
 # What is the output? (i.e. What should the code return?)
+
+#The output is the integer, properly formatted with commas.
+
 # What are the steps needed to solve the problem?
 
+#Determine whether the integer is long enough to require commas
+#If it is, insert a comma before the final three characters, and every three back
+#before that until reach its beginning.
 
 # 2. Initial Solution
 
